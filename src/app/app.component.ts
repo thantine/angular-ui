98b73c6c368558ad2e0476bdfbf8153e62556d6b
quangvinh <vinhbq1902@gmail.com>
2019-05-13 01:07:28 +0700

    initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { MenuItem } from '@thantine/thantine-angular-ui';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-library-app';
+  public avatarUrl = 'assets/images/avatar.png';
+  public logoUrl = 'assets/images/logo.png';
+
+  public menuItems: MenuItem[] = [
+    {href: '/menu-item-1', text: 'Menu item 1'},
+    {href: '/menu-item-2', text: 'Menu item 2'},
+  ];
+
+  public userMenuItems: MenuItem[] = [
+    {href: '/user-item-1', text: 'user item 1'},
+    {href: '/user-item-2', text: 'user item 2'},
+  ];
+
+  public sidebarItems: MenuItem[] = [
+    {href: '/sidebar-item-1', text: 'sidebar item 1'},
+    {href: '/sidebar-item-2', text: 'sidebar item 2'},
+  ];
 }
